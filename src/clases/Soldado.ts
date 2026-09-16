8cbbf11eb957f460217e4a8848c83922341100b2
valenbasiuk <valenbasiukcls@gmail.com>
2026-09-16 17:14:17 -0300

    clase de soldado

---
+++ src/clases/Soldado.ts
@@ -0,0 +1,29 @@
+import { Arma } from "../armas/Arma";
+
+export class Soldado {
+  private arma?: Arma;
+  private vida: number;
+
+  constructor(vida: number = 100, arma?: Arma) {
+    this.vida = vida;
+    this.arma = arma;
+  }
+
+  disparar(objetivo?: Soldado): number {
+    const dano = this.arma ? this.arma.disparar() : 10;
+    if (objetivo) {
+      objetivo.recibirDisparo(dano);
+    }
+    return dano;
+  }
+
+  recibirDisparo(dano: number): void {
+    this.vida -= dano;
+  }
+
+  estaVivo(): boolean {
+    return this.vida > 0;
+  }
+}
+
+export default Soldado;
